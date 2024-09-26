bdf7f72f7c7bcc62c90d6ac579a62a231e0bc4c7
Sara-AM91 <sara.am.hafezi@gmail.com>
2024-09-26 11:32:30 +0200

    Created all category-related components and updated server.js to integrate the new category routes.

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const dbinit = require("./db/dbinit");
+
 const userRoutes = require("./routes/userRoutes");
-const transactionRoutes = require("./routes/transactionRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
 
 dotenv.config();
 dbinit();
@@ -13,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/user", userRoutes);
-app.use("/transactions", transactionRoutes);
+app.use("/categories", categoryRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
